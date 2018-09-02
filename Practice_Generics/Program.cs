f4bf01e314c09d39a2671137fbaa08c381768487
gavogt <gvogt@asu.edu>
2018-09-02 12:30:23 -0700

    Add project files.

---
+++ Practice_Generics/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Practice_Gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greet the user
+            DisplayHeader();
+
+            // Init Lists
+            var carList = new List<Vehicle<int, DateTime>>();
+            var truckList = new List<Vehicle<double, int>>();
+
+            // If they want to initialize a new vehicle, ask for time
+            var vehicleOne = new Vehicle<int, DateTime>();
+            vehicleOne.Make = "Ferrari";
+            vehicleOne.Model = "488 GTB";
+            vehicleOne.Type = "Car";
+            vehicleOne.MPG = 22;
+            vehicleOne.VehicleAquiredDate = new DateTime(2018, 8, 16);
+
+            var vehicleTwo = new Vehicle<double, int>();
+            vehicleTwo.Make = "Volvo";
+            vehicleTwo.Model = "VNL64T430";
+            vehicleTwo.Type = "Truck";
+            vehicleTwo.MPG = 6.3;
+            vehicleTwo.VehicleAquiredDate = 2018;
+
+            // Loop here
+            carList.Add(vehicleOne);
+            truckList.Add(vehicleTwo);
+
+            // If they want to display a car list or a truck list option
+            foreach (var item in carList)
+            {
+                Console.WriteLine("Car make: " + item.Make);
+                Console.WriteLine("Car model: " + item.Model);
+                Console.WriteLine("Car MPG: " + item.MPG);
+                Console.WriteLine("Car Aquired: " + ((DateTime)(item.VehicleAquiredDate)).ToShortDateString());
+
+            }
+
+            foreach (var item in truckList)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Truck make: " + item.Make);
+                Console.WriteLine("Truck model: " + item.Model);
+                Console.WriteLine("Truck MPG: " + item.MPG);
+                Console.WriteLine("Truck Aquired: " + item.VehicleAquiredDate);
+            }
+
+            Console.WriteLine("");
+        }
+
+        public static void DisplayHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            // cw snippet
+            Console.WriteLine("\n****** WELCOME! ******\n");
+
+            Console.ResetColor();
+
+        }
+    }
+}
---
+++ Practice_Generics/Vehicle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Gen
+{
+    /// <summary>
+    /// A vehicle class that takes two generics
+    /// </summary>
+    /// <typeparam name="K">Can be an int or double</typeparam>
+    /// <typeparam name="V">Can be a DateTime or an int</typeparam>
+    class Vehicle<K, V>
+    {
+
+        // Propfull
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        // Propfull
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        // Propfull
+        private string type;
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        // Propfull
+        private K mpg;
+
+        public K MPG
+        {
+            get { return mpg; }
+            set { mpg = value; }
+        }
+
+        // Propfull
+        private V vehicleAquiredDate;
+
+        public V VehicleAquiredDate
+        {
+            get { return vehicleAquiredDate; }
+            set { vehicleAquiredDate = value; }
+        }
+
+    }
+}
